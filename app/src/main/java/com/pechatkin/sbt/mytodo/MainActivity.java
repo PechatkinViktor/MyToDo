906c157645be7b7f3d6882e60a03610d75a30ef7
Viktor <pechatkinvictor@gmail.com>
2019-11-29 14:32:05 +0300

    init

---
+++ app/src/main/java/com/pechatkin/sbt/mytodo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pechatkin.sbt.mytodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
